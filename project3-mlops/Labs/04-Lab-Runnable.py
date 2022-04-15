eea120ef2c0d435bf04703551f3565a533dcedbb
qfu4 <qfu4>
2022-04-15 21:51:39 +0000

    removed unnecessary command

--- project3-mlops/Labs/04-Lab-Runnable.py
+++ project3-mlops/Labs/04-Lab-Runnable.py
@@ -75,10 +75,6 @@ dbutils.notebook.exit(json.dumps({
 
 # COMMAND ----------
 
-df
-
-# COMMAND ----------
-
 # MAGIC %md-sandbox
 # MAGIC &copy; 2020 Databricks, Inc. All rights reserved.<br/>
 # MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="http://www.apache.org/">Apache Software Foundation</a>.<br/>
